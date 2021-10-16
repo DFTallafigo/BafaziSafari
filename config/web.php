8cf2144b31281b5461912bd20903d4ffe029c98d
David Florido Tallafigo <davidtallafigo@gmail.com>
2021-10-16 17:54:26 +0200

    urlManager

--- config/web.php
+++ config/web.php
@@ -51,14 +51,18 @@ $config = [
         'formatter' => [
             'timeZone' => 'Europe/Madrid',
         ],
-        /*
         'urlManager' => [
-            'enablePrettyUrl' => true,
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
             'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+               '<controller:\w+>/<id:\d+>' => '<controller>/view',
+               '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+               '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+         ],
     ],
     'container' => [
         'definitions' => [
