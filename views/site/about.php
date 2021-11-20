6cdd87a76404afdf33bd24ba43b3fc965427ccc8
David Florido Tallafigo <davidtallafigo@gmail.com>
2021-11-20 17:40:58 +0100

    about

--- views/site/about.php
+++ views/site/about.php
@@ -10,10 +10,56 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
     <div class="about-us">
-        
+        <h1>
+            <strong id="welcome">
+                ABOUT US
+            </strong>
+        </h1>
     </div>
 
-    <div class="content-about">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 about-title">
+                <strong>
+                    <p class="services">
+                        SERVICES WE DO
+                    </p>
+                </strong>
+            </div>
+            <div class="col-6">
+                <div class="card">
+                    <img class="card-img-top" src="https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/254960425_191840889784801_5894135393508198409_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=973b4a&_nc_ohc=23e-Y7LzxXUAX_0oEAP&_nc_ht=scontent-mad1-1.xx&oh=da751da30214aba00ffdb0df0e592d6b&oe=619DCE72" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">TRANSFERS</h5>
+                        <p class="card-text">
+                            We offer transfer services from Airports to and from Lodges and hotels,we cover the following airports:
+                        </p>
+                        <ul>
+                            <li class="card-list">Tambo internacional airport</li>
+                            <li class="card-list">Kruger mpumalanga internacional airport</li>
+                            <li class="card-list">Eastgate airport</li>
+                            <li class="card-list">Skukuza airport</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-6">
+                <div class="card">
+                    <img class="card-img-top" src="https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/215578226_121475343488023_5865853411164028403_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=973b4a&_nc_ohc=lYNy-hvca5EAX8qrjy4&_nc_ht=scontent-mad1-1.xx&oh=a405fde899bcae2c79262483f089c75c&oe=619E7CDF" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">GAME RESERVES</h5>
+                        <ul>
+                            <li class="card-list">Sabie sands</li>
+                            <li class="card-list">Timbavati</li>
+                            <li class="card-list">Balule</li>
+                            <li class="card-list">Karongwe</li>
+                            <li class="card-list">Kruger National park</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 
